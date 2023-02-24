9b2f3f6fc65d53d566b0c64a97b6f0ccadd40ad0
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-02-24 16:12:33 +0200

    Add unittest for testing user model

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+# TESTING USER MODEL
+
+class UserTesting(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(username="abdelrahman", email="abdelrahman@gmail.com",
+                                    role="user", password="passwrd")
+        self.assertEqual(user.username, "abdelrahman")
+        self.assertEqual(user.email, "abdelrahman@gmail.com")
+        self.assertEqual(user.role, "user")
