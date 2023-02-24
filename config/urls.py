9363894eade1d264103acf7de620bb3dc6a21670
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-02-24 17:05:46 +0200

    Setup static and media files

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 # SETUP SWAGGER 
 schema_view = get_schema_view(
@@ -23,4 +25,6 @@ urlpatterns = [
    path('clinic/', include('clinic.urls')),
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
-
+# ADD STATIC FILES
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
