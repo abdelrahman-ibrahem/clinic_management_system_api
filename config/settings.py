c5b6378e99de7e4be3e573ce28a613a9cb7480f9
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-03-05 14:36:28 +0200

    get static files

--- config/settings.py
+++ config/settings.py
@@ -167,11 +167,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'assests') 
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static') 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
