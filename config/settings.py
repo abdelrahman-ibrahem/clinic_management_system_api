700759ee37f353d89f9d14a245123d63cad1775a
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-03-05 14:51:05 +0200

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,7 @@ REST_AUTH  = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -90,6 +91,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS=True
+
 
 ROOT_URLCONF = 'config.urls'
 
